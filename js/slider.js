4f85d8928968791eace4f17004b5a025eda3c58b
Bohdan Masliannykov <masliannykov@mail.ru>
2016-11-02 21:48:33 +0100

    Fix navbar

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var mySwiper = new Swiper ('.swiper-container', {
+    slidesPerView:3,
+    loop:true,
+    spaceBetween:28,
+    nextButton: '.arrow-right',
+    prevButton: '.arrow-left',
+  })
+});
