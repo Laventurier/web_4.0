5312c2416e2a3d01b60bb4014515a98fe8266284
Bohdan Masliannykov <masliannykov@mail.ru>
2016-11-03 22:36:07 +0100

    final steps, must fix navbar toggle

--- js/slider.js
+++ js/slider.js
@@ -6,4 +6,35 @@ $(document).ready(function(){
     nextButton: '.arrow-right',
     prevButton: '.arrow-left',
   })
+  $('.bakery-intro').children('h1').css({'position':'relative','left': '-400px',"opacity":"0"}).animate({
+    "left":"0",
+    "opacity":"1"
+  },400);
+  $('.bakery-intro').children('span').css({'display':'block','position':"relative",'right': '-400px',"opacity":"0"}).animate({
+    "right":"0",
+    "opacity":"1"
+  },400);
+  $('.bakery-intro').children('button').css({
+    'visibility':'visible',
+}).animate({
+    opacity:1
+  },800);
+
+var isHide=false;
+$('.nav-tab').click(function () {
+  if(isHide===false){
+    var width = $('.dish').width();
+    $('.dish').animate({
+      left:width+3
+    },500);
+    return isHide=true;
+  }else{
+    $('.dish').animate({
+      left:0
+    },500);
+    return isHide=false;
+  }
+
+
+})
 });
